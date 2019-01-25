e7ea97421c30425a3d7e715cb095c892bf8dd020
umuzi <lgmohale@outlook.com>
2019-01-25 13:52:22 +0200

    admin page done and tested

--- frontend/main.js
+++ frontend/main.js
@@ -3,13 +3,19 @@ function display(){
     .then(res => res.json())
     .then(res => res.map(user => user))
 .then(userNames =>{
-var entryName = "<tr><th>Date Submitted</th> <th>Country</th> <th>Organisation</th> <th>Accept</th><th>Rejected</th></tr>"
-  entryName += userNames.map(data=>`<tr> <td>${data.datesubmitted}</td>
+var entryName = "<tr><th>No.</th> <th>Date</th> <th>Country</th> <th>Name</th> <th>Website</th> <th>Contact</th> <th>Features</th> <th>Courses</th> <th>Age Group</th> <th>Accept</th><th>Rejected</th></tr>"
+  entryName += userNames.map(data=>`<tr>
+  <td>${data.id}</td> 
+  <td>${data.datesubmitted}</td>
     <td> ${data.country}</td>
-    ${data.organisationemail}
-    <td> ${data.organisationame}</td> ${data.id}
-    <td><button class="accept" onclick="UpdateApprove(${data.id})">Accept</button> </td>
-    <td> <button class="reject" onclick="UpdateReject(${data.id})">Reject</button><br></td></tr>`).join('')
+    <td>${data.organisationame}</td>
+    <td> ${data.website}</td>
+    <td> ${data.personscontactemail}</td>
+    <td> ${data.features}</td> 
+    <td> ${data.coursesoffered}</td>  
+    <td> ${data.agegroups}</td>
+    <td><button class="acceptButton" onclick="UpdateApprove(${data.id})">Accept</button> </td>
+    <td> <button class="rejectButton" onclick="UpdateReject(${data.id})">Reject</button><br></td></tr>`).join('')
 document.getElementById("entry").innerHTML = entryName;
 
 });
@@ -20,13 +26,19 @@ function pending(){
     .then(res => res.json())
     .then(res => res.map(user => user))
 .then(userNames =>{
-  var entryName ="<tr><th>Date Submitted</th> <th>Country</th> <th>Organisation</th> <th>Accept</th><th>Rejected</th></tr>"
-entryName += userNames.map(data=>`<tr><td>${data.datesubmitted}</td>
-  <td> ${data.country}</td>
-  ${data.organisationemail}
-  <td> ${data.organisationame}</td>${data.id}
-  <td> <button onclick="UpdateApprove(${data.id})">Accept</button></td>
-  <td> <button onclick="UpdateReject(${data.id})">Reject</button><br><td></tr>`).join('')
+    var entryName = "<tr><th>No.</th> <th>Date</th> <th>Country</th> <th>Name</th> <th>Website</th> <th>Contact</th> <th>Features</th> <th>Courses</th> <th>Age Group</th> <th>Accept</th><th>Rejected</th></tr>"
+    entryName += userNames.map(data=>`<tr>
+     <td>${data.id}</td>
+     <td>${data.datesubmitted}</td>
+      <td> ${data.country}</td>
+      <td>${data.organisationame}</td>
+      <td> ${data.website}</td>
+      <td> ${data.personscontactemail}</td>
+      <td> ${data.features}</td> 
+      <td> ${data.coursesoffered}</td>  
+      <td> ${data.agegroups}</td> 
+  <td> <button class="acceptButton" onclick="UpdateApprove(${data.id})">Accept</button></td>
+  <td> <button class="rejectButton" onclick="UpdateReject(${data.id})">Reject</button><br><td></tr>`).join('')
 document.getElementById("entry").innerHTML = entryName;
 });
 }
@@ -36,13 +48,18 @@ function approved(){
     .then(res => res.json())
     .then(res => res.map(user => user))
 .then(userNames =>{
-  var entryName = "<tr><th>Date Submitted</th> <th>Country</th> <th>Organisation</th> <th>Reject</th></tr>"
-entryName += userNames.map(data=>`<tr><td>${data.datesubmitted} </td>
-  <td>${data.country}</td>
-  ${data.organisationemail}
-  <td>${data.organisationame} </td>
-
-  <td> <button onclick="UpdateReject(${data.id})">Reject</button><br></td></tr>`).join('')
+    var entryName = "<tr><th>No.</th> <th>Date</th> <th>Country</th> <th>Name</th> <th>Website</th> <th>Contact</th> <th>Features</th> <th>Courses</th> <th>Age Group</th> <th>Rejected</th></tr>"
+    entryName += userNames.map(data=>`<tr> 
+    <td>${data.id}</td>
+    <td>${data.datesubmitted}</td>
+      <td> ${data.country}</td>
+      <td>${data.organisationame}</td>
+      <td> ${data.website}</td>
+      <td> ${data.personscontactemail}</td>
+      <td> ${data.features}</td> 
+      <td> ${data.coursesoffered}</td>  
+      <td> ${data.agegroups}</td>  
+  <td> <button class="rejectButton" onclick="UpdateReject(${data.id}) ">Reject</button><br></td></tr>`).join('')
    document.getElementById("entry").innerHTML = entryName;
 });
 }
@@ -52,12 +69,18 @@ function rejected(){
     .then(res => res.json())
     .then(res => res.map(user => user))
 .then(userNames => {
-var entryName = "<tr><th>Date Submitted</th> <th>Country</th> <th>Organisation</th> <th>Accept</th></tr>"
-entryName += userNames.map(data=>`<tr><td>${data.datesubmitted} </td>
-  <td>${data.country}</td>
-   ${data.organisationemail}
-   <td>${data.organisationame}</td>
-    <td><button onclick="UpdateApprove(${data.id})">Accept</button>  <br></td></tr>`).join('')
+    var entryName = "<tr><th>No.</th> <th>Date</th> <th>Country</th> <th>Name</th> <th>Website</th> <th>Contact</th> <th>Features</th> <th>Courses</th> <th>Age Group</th> <th>Accept</th></tr>"
+    entryName += userNames.map(data=>`<tr> 
+    <td>${data.id}</td>
+    <td>${data.datesubmitted}</td>
+      <td> ${data.country}</td>
+      <td>${data.organisationame}</td>
+      <td> ${data.website}</td>
+      <td> ${data.personscontactemail}</td>
+      <td> ${data.features}</td> 
+      <td> ${data.coursesoffered}</td>  
+      <td> ${data.agegroups}</td> 
+    <td><button class="acceptButton" onclick="UpdateApprove(${data.id})">Accept</button>  <br></td></tr>`).join('')
   document.getElementById("entry").innerHTML = entryName;
 });
 }
@@ -78,6 +101,7 @@ function UpdateApprove(id){
         fetch('http://localhost:5955/user/UpdateApprove', options)
         .then(res => res.json())
         .then(res => console.log(res));
+        alert();
 }
 
 
@@ -95,4 +119,18 @@ function UpdateReject(id){
         fetch('http://localhost:5955/user/UpdateReject', options)
         .then(res => res.json())
         .then(res => console.log(res));
+        alert();
 }
+
+function Refresh(){
+    fetch('http://localhost:5955/user/update')
+    .then(res => res.json())
+    .then(res => res.map(user => user))
+    alert();
+    refreshPage();
+}
+
+function refreshPage(){
+    window.location.reload();
+    return false;
+} 
